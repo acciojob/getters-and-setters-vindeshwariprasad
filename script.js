7028da07592a2d5be9b51563adbc27c49e7212d8
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 04:18:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name = name;
-		this.age = age;
+		this._name = name;
+		this._age = age;
 	}
 	get name(){
-		return this.name;
+		return this._name;
 	}
-	set age(ag){
-		this.age = ag;
+	set age(age){
+		this._age = age;
 	}
 }
 
